37245a41190b625b5f4f5dc8726bf58e8cd46a5e
martincz <martincz@mokeedev.com>
2016-12-04 17:06:59 +0800

    smdk4412-common: mokee bringup

--- BoardCommonConfig.mk
+++ BoardCommonConfig.mk
@@ -68,8 +68,8 @@ BOARD_FLASH_BLOCK_SIZE := 4096
 TARGET_USERIMAGES_USE_EXT4 := true
 
 # Hardware tunables
-BOARD_HARDWARE_CLASS := hardware/samsung/cmhw \
-    device/samsung/smdk4412-common/cmhw
+BOARD_HARDWARE_CLASS := hardware/samsung/mkhw \
+    device/samsung/smdk4412-common/mkhw
 
 # Graphics
 BOARD_EGL_NEEDS_HANDLE_VALUE := true
