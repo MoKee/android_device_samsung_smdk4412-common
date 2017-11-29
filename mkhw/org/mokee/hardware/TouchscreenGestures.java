f021dd8beb33624033ebfc5b1cebe34c987e8de5
martincz <martincz@mokeedev.com>
2017-11-29 20:22:34 +0800

    smdk4412-common: Derp
    Change-Id: I8fb16acc633121133b0dbea7e81896d2ce1d2a65

--- mkhw/org/mokee/hardware/TouchscreenGestures.java
+++ mkhw/org/mokee/hardware/TouchscreenGestures.java
@@ -1,6 +1,7 @@
 /*
  * Copyright (C) 2016 The CyanogenMod Project
  * Copyright (C) 2017 The LineageOS Project
+ * Copyright (C) 2017 The MoKee Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,11 +16,11 @@
  * limitations under the License.
  */
 
-package org.cyanogenmod.hardware;
+package org.mokee.hardware;
 
-import org.cyanogenmod.internal.util.FileUtils;
+import org.mokee.internal.util.FileUtils;
 
-import cyanogenmod.hardware.TouchscreenGesture;
+import mokee.hardware.TouchscreenGesture;
 
 /**
  * Touchscreen gestures API
